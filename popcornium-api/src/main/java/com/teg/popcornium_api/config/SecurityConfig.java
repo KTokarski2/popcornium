3b953598f58d7a1cacc47d5169d13bc7bdb1ea51
bawa <bawa@netcompany.com>
2025-12-03 08:45:46 +0100

    Removed unnecessary line in SecurityConfig.

--- popcornium-api/src/main/java/com/teg/popcornium_api/config/SecurityConfig.java
+++ popcornium-api/src/main/java/com/teg/popcornium_api/config/SecurityConfig.java
@@ -13,7 +13,6 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/files/**").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
